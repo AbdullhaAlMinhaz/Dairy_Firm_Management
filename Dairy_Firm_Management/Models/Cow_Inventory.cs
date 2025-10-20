3c543d2e23b9da10f352806436dcdc4f5a8a0f08
Abdullha Al Minhaz <97035413+AbdullhaAlMinhaz@users.noreply.github.com>
2025-10-20 21:43:52 +0600

    Add project files.

---
+++ Dairy_Firm_Management/Models/Cow_Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dairy_Firm_Management.Models
+{
+    public class Cow_Inventory
+    {
+        [Key]
+        public int Cow_Id { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public double Weight { get; set; }
+        public double Price { get; set; }
+        public string Breed { get; set; }
+        public DateTime EntryDate { get; set; }
+
+
+    }
+}
+
---
+++ Dairy_Firm_Management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dairy_Firm_Management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
